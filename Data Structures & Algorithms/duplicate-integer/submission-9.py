c54f3a7bc24f3bc1621a77159dd5406ef50018e8
Yebon Byun <96388412+Yebon-Byun@users.noreply.github.com>
2026-06-01 16:45:06 -0700

    Add: duplicate-integer - submission-9

---
+++ Data Structures & Algorithms/duplicate-integer/submission-9.py
@@ -0,0 +1,17 @@
+class Solution:
+    # Brute Force
+    # def hasDuplicate(self, nums: List[int]) -> bool:
+    #     for i in range(len(nums)):
+    #         for j in range(i + 1, len(nums)):
+    #             if nums[i] == nums[j]:
+    #                 return True
+    #     return False
+
+    # Hash Set
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        sorted_nums = sorted(nums)
+        for i in range(1, len(nums)):
+            if sorted_nums[i] == sorted_nums[i - 1]:
+                return True
+        return False
+
