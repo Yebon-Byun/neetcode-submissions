5146795fc19bfa9c399523899f0a17ab9b74d1f5
Yebon Byun <96388412+Yebon-Byun@users.noreply.github.com>
2026-08-20 09:58:35 -0700

    Add: two-integer-sum - submission-64

---
+++ Data Structures & Algorithms/two-integer-sum/submission-64.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for idx, val in enumerate(nums):
+            comp = target - val
+            if comp in seen:
+                return [seen[comp], idx]
+            seen[val] = idx
+        return []
+
+                
