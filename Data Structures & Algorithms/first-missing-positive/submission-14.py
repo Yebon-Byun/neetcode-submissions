35697b2923c21f5d5262a923cabe19c4e3e7258f
Yebon Byun <96388412+Yebon-Byun@users.noreply.github.com>
2026-08-27 10:54:12 -0700

    Add: first-missing-positive - submission-14

---
+++ Data Structures & Algorithms/first-missing-positive/submission-14.py
@@ -0,0 +1,17 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        missing = 1
+        while True:
+            flag = True
+            for num in nums:
+                if missing == num:
+                    flag = False
+                    break
+                
+            if flag:
+                return missing
+            missing += 1
+    
+    # time: O(n²)
+    # space: O(1)
+    
